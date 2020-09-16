cc865250183a73b746056654e836b330ea8e435d
Matsuo <35791279+matsuot-cu@users.noreply.github.com>
2020-09-16 03:25:47 -0600

    Adding archi_comments.cpp

---
+++ Week4/archi_comment.cpp
@@ -0,0 +1,38 @@
+// archi.cpp
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+
+int main()
+{
+	// Declare and initialize variables 
+    string goAgain;
+
+
+	do{
+		// Ask for user input
+	
+	
+
+        // Calculate the volume
+		
+		
+		
+		// Calculate the buoyancy force
+	
+	
+	
+		// Check to see whether buoyancy force is greater than weight
+	
+	
+
+		// Ask for user input
+	
+	
+
+	}while(goAgain != "no"); //Check the condtion is met
+
+	return 0;
+	
+}
