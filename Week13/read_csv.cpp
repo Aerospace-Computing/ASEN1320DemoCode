81de3531952187eb44285ff7b7245c0147262614
Matsuo <35791279+matsuot-cu@users.noreply.github.com>
2020-12-02 06:43:42 -0700

    Add files via upload

--- Week13/read_csv.cpp
+++ Week13/read_csv.cpp
@@ -1,22 +1,23 @@
 #include <iostream> 
 #include <fstream>
 #include <string>
-
 using namespace std;
 
 int main() {
 
- ifstream File;
- File.open("file.csv");
+ ifstream F;
+ F.open("file.csv");
  string stringX, stringY;
  
  for( int i=0; i<10; i++){
-	 getline(File, stringX, ',' );
-     getline(File, stringY );
+	 getline(F, stringX, ',' );
+  getline(F, stringY );
 	 double a = stod(stringX);
 	 double b = stod(stringY);
 	 cout << "X: " << a  << " Y: " << b << endl;
  }
-	
-  return 0;
+ 
+ F.close();
+ 
+ return 0;
 }
